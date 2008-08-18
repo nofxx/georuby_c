64dd21c977a5376d7afa141badbf66cbfaf8b678
Marcos Augusto <x@nofxx.com>
2008-08-17 21:55:12 -0300

    setup autotest

--- lib/georuby_c.rb
+++ lib/georuby_c.rb
@@ -1,6 +1,15 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'georuby_c/ext/geo'
+require 'ext/geo'
+
 module GeorubyC  
+  include Geo
+  
+  
+  
+  
+  
+  
+  
 end
