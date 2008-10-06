bb6dee284e7c564da3a7e01959382bb0a091eb2a
Marcos Augusto <x@nofxx.com>
2008-10-06 05:33:26 -0300

    sync

--- ext/distance.c
+++ ext/distance.c
@@ -1,12 +1,11 @@
 #include <common.h>
 
-VALUE rb_distance;
-
+VALUE rb_distance_haversine;
+// Haversine formula.
 double HaversineDistance( double y1, double x1, double y2, double x2 )
 {
   double nRadius = 6370997.0; // Earth’s radius in Kilometers
   double rad = 0.0174532925199433;
-	// Get the difference between our two points in radians
 	double nDLat = (y2 - y1) * rad;
 	double nDLon = (x2 - x1) * rad;
 	y1 *= rad;
@@ -18,70 +17,87 @@ double HaversineDistance( double y1, double x1, double y2, double x2 )
 	return nD;
 }
 
-//
-/*double PythagoreanDistance( double y1, double x1, double y2, double x2 )*/
-/*{*/
-/*  double nR = sqrt( pow(x2 - x1) + pow( y2 - y1 ));*/
-/*  return nR;*/
-/*}*/
+VALUE rb_distance_euclidian;
+// Euclidian, nice guy.
+double EuclidianDistance( double y1, double x1, double y2, double x2 )
+{
+  double nR = sqrt( pow(x2 - x1, 2) + pow( y2 - y1, 2));
+  return nR;
+}
 
-/*double FlatEarthDistance(double y1, double x1, double y2, double x2 )*/
-/*{*/
-/*  double rad = 1.570796326794897;*/
-/*  double nRadius = 6370997.0; // Earth’s radius in Kilometers*/
-/*  double nA = rad - y1;*/
-/*  double nB = rad - y2;*/
-/*  double nC = sqrt( pow(nA) + pow(nB) - 2 * nA * nB * cos(x2 - x1));*/
-/*  double nR = nRadius * nC;*/
-/*  return nR;*/
-/*}*/
+VALUE rb_distance_flat_earth;
+// Afterall, the earth is flat!
+// =P
+double FlatEarthDistance(double y1, double x1, double y2, double x2 )
+{
+  double rad = 1.570796326794897;
+  double nRadius = 6370997.0; // Earth’s radius in Kilometers
+  double nA = rad - y1;
+  double nB = rad - y2;
+  double nC = sqrt( pow(nA,2) + pow(nB,2) - 2 * nA * nB * cos(x2 - x1));
+  double nR = nRadius * nC;
+  return nR;
+}
 
+VALUE rb_distance_ellipsoidal;
 // Using Vincenty Formula
 //a is the semi-major axis (equatorial radius) of the ellipsoid
 // b is the semi-minor axis (polar radius) of the ellipsoid
 // Their values by default are set to the ones of the WGS84 ellipsoid
-/*double EllipsoidalDistance(double y1, double x1, double y2, double x2, double a, double b )*/
-/*{*/
-/*  double a = 1;*/
-/*  double rad = 0.0174532925199433;*/
-/*  double f = (a-b)/a;*/
-/*  double l, lambda = (x2 - x1) * rad;*/
-/*  double u1 = atan((1-f) * tan(y1 * rad));*/
-/*  double u2 = atan((1-f) * tan(y2 * rad));*/
-/*  double sinU1 = sin(u1);*/
-/*  double cosU1 = cos(u1);*/
-/*  double sinU2 = sin(u2);*/
-/*  double cosU2 = cos(u2);*/
-/*  double lambdaP = 6.283185307179586;*/
-/*  int iterLimit = 20;*/
+double EllipsoidalDistance(double y1, double x1, double y2, double x2, double a, double b )
+{
+	a = 6378137.0;
+  double rad = 0.0174532925199433;
+  double f = (a-b)/a;
+  double l = (x2 - x1) * rad;
 
-/*  while(abs(lambda-lambdaP) > 1e-12 && iterLimit > 0) {*/
-/*    double sinLambda = sin(lambda);*/
-/*    double cosLambda = cos(lambda);*/
-/*    double sinSigma = sqrt((cosU2*sinLambda) * (cosU2*sinLambda) + (cosU1*sinU2-sinU1*cosU2*cosLambda) * (cosU1*sinU2-sinU1*cosU2*cosLambda));*/
-/*    if (sinSignma == 0) {*/
-/*      return 0;*/
-/*    }*/
-/*    double cosSigma = sinU1*sinU2 + cosU1*cosU2*cosLambda;*/
-/*    double sigma = atan2(sinSigma, cosSigma);*/
-/*    double sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;*/
-/*    double cosSqAlpha = 1 - sinAlpha*sinAlpha;*/
-/*    double cos2SigmaM = cosSigma - 2*sinU1*sinU2/cosSqAlpha;*/
-/*    if (nan(cos2SigmaM)){*/
-/*      cos2SigmaM = 0;  //#equatorial line: cosSqAlpha=0*/
-/*    }*/
-/*    double c = f/16*cosSqAlpha*(4+f*(4-3*cosSqAlpha));*/
-/*    lambdaP = lambda;*/
-/*    lambda = l + (1-c) * f * sinAlpha * (sigma + c * sinSigma * (cos2SigmaM + c * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM)));*/
-/*    return lambda*/
-/*  }*/
-/*  if(iterLimit == 0){*/
-/*   return 0;*/
-/*  }*/
-/*  double uSq = cosSqAlpha * (a*a - b*b) / (b*b);*/
-/*  double a_bis = 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)));*/
-/*  double b_bis = uSq/1024 * (256+uSq*(-128+uSq*(74-47*uSq)));*/
-/*  double deltaSigma = b_bis * sinSigma*(cos2SigmaM + b_bis/4*(cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)- b_bis/6*cos2SigmaM*(-3+4*sinSigma*sinSigma)*(-3+4*cos2SigmaM*cos2SigmaM)));*/
-/*  double final = b*a_bis*(sigma-deltaSigma);*/
-/*  return final;*/
-/*}*/
+  double u1 = atan((1-f) * tan(y1 * rad));
+  double u2 = atan((1-f) * tan(y2 * rad));
+  double sinU1 = sin(u1);
+  double cosU1 = cos(u1);
+  double sinU2 = sin(u2);
+  double cosU2 = cos(u2);
+
+  double lambdaP = 6.283185307179586;
+  int iter_limit = 20;
+
+  double sin_sigma;
+	double cos_sq_alpha;
+	double cos_sigma_m;
+ 	double cos_sigma ;
+ 	double sigma;
+
+  while(abs(l-lambdaP) > 1e-12 && iter_limit > 0) {
+    iter_limit--;
+    double sin_lambda = sin(l);
+    double cos_lambda = cos(l);
+    sin_sigma = sqrt((cosU2*sin_lambda) * (cosU2*sin_lambda) + (cosU1*sinU2-sinU1*cosU2*cos_lambda) * (cosU1*sinU2-sinU1*cosU2*cos_lambda));
+    if (sin_sigma == 0)
+    {
+    	return 0;
+    }
+    cos_sigma = sinU1*sinU2 + cosU1*cosU2*cos_lambda;
+    sigma = atan2(sin_sigma, cos_sigma);
+    double sin_alpha = cosU1 * cosU2 * sin_lambda / sin_sigma;
+    cos_sq_alpha = 1 - sin_alpha*sin_alpha;
+    cos_sigma_m = cos_sigma - 2*sinU1*sinU2/cos_sq_alpha;
+    if ( cos_sigma_m == NAN )
+    {
+      cos_sigma_m = 0;  //#equatorial line: cos_sq_alpha=0
+    }
+    double c = f/16*cos_sq_alpha*(4+f*(4-3*cos_sq_alpha));
+
+    l = l + (1-c) * f * sin_alpha * (sigma + c * sin_sigma * (cos_sigma_m + c * cos_sigma * (-1 + 2 * cos_sigma_m * cos_sigma_m)));
+    return l;
+  }
+  if(iter_limit == 0)
+  {
+   return 0;
+  }
+  double uSq = cos_sq_alpha * (a*a - b*b) / (b*b);
+  double a_bis = 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)));
+  double b_bis = uSq/1024 * (256+uSq*(-128+uSq*(74-47*uSq)));
+  double deltaSigma = b_bis * sin_sigma*(cos_sigma_m + b_bis/4*(cos_sigma*(-1+2*cos_sigma_m*cos_sigma_m)- b_bis/6*cos_sigma_m*(-3+4*sin_sigma*sin_sigma)*(-3+4*cos_sigma_m*cos_sigma_m)));
+  double final = b*a_bis*(sigma-deltaSigma);
+  return final;
+}
--- spec/georuby_c_spec.rb
+++ spec/georuby_c_spec.rb
@@ -1,23 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-
 # Time to add your specs!
 # http://rspec.info/
 describe "Place your specs here" do
-  
+
   it "should instantiate Geometry" do
     @geo = Geometry.new
     @geo.class.should eql(Geometry)
   end
-  
+
   it "should instantiate Point" do
     @point = Point.new
     @point.should be_instance_of(Point)
   end
-  
+
   it "should instantiate Line" do
     @line = LineString.new
     @line.should be_instance_of(LineString)
   end
-  
+
 end
